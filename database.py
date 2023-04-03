15364d0f715b3a81af24eb7a7330af80ab10ede6
hevardhan <hevardhan2004@gmail.com>
2023-04-04 01:10:06 +0530

    Minutes Changes are been done

--- database.py
+++ database.py
@@ -24,3 +24,36 @@ td = {
     "Amount": f"{amount}",
     "Transaction Method":f"{method}"
 }
+
+def display_date():
+    date_dis = date.today()
+    if date_dis.month == 1:
+        month = "Jan"
+    elif date_dis.month == 2:
+        month = "Feb"
+    elif date_dis.month == 3:
+        month = 'Mar'
+    elif date_dis.month == 4:
+        month = 'Apr'
+    elif date_dis.month == 5:
+        month = 'May'
+    elif date_dis.month == 6:
+        month = 'Jun'
+    elif date_dis.month == 7:
+        month = 'Jul'
+    elif date_dis.month == 8:
+        month = 'Aug'
+    elif date_dis.month == 9:
+        month = 'Sep'
+    elif date_dis.month == 10:
+        month = 'Oct'
+    elif date_dis.month == 11:
+        month = 'Nov'
+    elif date_dis.month == 12:
+        month = 'Dec'
+    else:
+        raise Exception("Error While Getting Date")
+
+    disp_var = f"{date_dis.day} {month}"
+    return disp_var
+print(display_date())
