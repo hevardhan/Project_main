bd921d868ccbb0bb4b20e3b0c9f1bec4710b3e4f
cvkchris <chris11.vinod@gmail.com>
2023-05-04 22:48:51 +0530

    added graphs into excel

--- fire.py
+++ fire.py
@@ -6,7 +6,7 @@ from firebase_admin import db, auth, _auth_utils
 from database import display_date,date
 from openpyxl.workbook.protection import WorkbookProtection
 from openpyxl import Workbook
-from openpyxl.chart import DoughnutChart, Reference
+from openpyxl.chart import DoughnutChart, BarChart, Reference
 import os.path
 
 # Firebase Configurationcand Credentials---------------------------------------------------------------
@@ -21,7 +21,7 @@ firebaseConfig = {
   "measurementId": "G-6EYJ0B0PXS"
 }
 
-cred = credentials.Certificate('budget-buddy-key.json')
+cred = credentials.Certificate('D:\PROJECTS\Budget Buddy\Project\Project_main\\budget-buddy-key.json')
 
 # Initializing Firebase------------------------------------------------------------------
 f_admin_init(cred, {"databaseURL": "https://budget-buddy-11-default-rtdb.firebaseio.com"})
@@ -177,8 +177,6 @@ def monthly_balance(userid):
     trans_ref = db.reference(f'Userdata/{userid}/Transaction')
     trans_details = trans_ref.order_by_key().get()
     
-    sum_inc = 0
-    sum_exp = 0
 
     months = ["Jan","Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     bal = [0,0,0,0,0,0,0,0,0,0,0,0]
@@ -192,9 +190,27 @@ def monthly_balance(userid):
                             bal[i] += int(trans_details[date][key]['Amount'])
                         else:
                             bal[i] -= int(trans_details[date][key]['Amount'])
-    print(bal)
+    
     return bal
 
+def monthly_expense(userid):
+    trans_ref = db.reference(f'Userdata/{userid}/Transaction')
+    trans_details = trans_ref.order_by_key().get()
+
+    months = ["Jan","Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    exp = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+    for date in trans_details:
+        for i in range(1,12):
+            if months[i] in date:
+                for key in trans_details[date]:
+                    if key.isnumeric():
+                        if trans_details[date][key]['Type'] == "Expense":
+                            exp[i] += int(trans_details[date][key]['Amount'])
+                        else:
+                            pass
+
+    return exp
 
 # Getting Display Name ------------------------------------------------------------------
 def get_display_name(uid):
@@ -226,6 +242,7 @@ def expense_per_day(user_id, date):
 
     return sum_exp
 
+
 # Export Data ------------------------------------------------------------
 def export(user_id, pswd,path):
     trans_ref = db.reference(f'Userdata/{user_id}/Transaction')
@@ -237,8 +254,12 @@ def export(user_id, pswd,path):
     t_no = name_data['Number of Transactions']
 
     wb = Workbook()
-    ws = wb.active
+    ws = wb.active.title("Main")
     ws1 = wb.create_sheet("Expenses")
+    ws2 = wb.create_sheet("Monthly Balance")
+    ch1 = wb.create_chartsheet("Daily Average")
+    ch2 = wb.create_chartsheet("Weekly Average")
+    ch3 = wb.create_chartsheet("Monthly Expense")
 
     # Worksheet -----------------------
     ws.merge_cells('A1:F1')
@@ -281,19 +302,19 @@ def export(user_id, pswd,path):
                 i +=1
                 j +=1
 
-    # Worksheet 1 -----------------------
-    ws1.merge_cells('A1:F1')
+    # Expenses  -----------------------
+    ws1.merge_cells('A1:E1')
     cell1 = ws1['A1']
     cell1.value = f'Expenses'
 
 
-    ws1['A3'] = 'SNO'
-    ws1['B3'] = 'Date'
-    ws1['C3'] = 'Description'
-    ws1['D3'] = 'Category'
-    ws1['E3'] = 'Amount'
+    ws1['A2'] = 'SNO'
+    ws1['B2'] = 'Date'
+    ws1['C2'] = 'Description'
+    ws1['D2'] = 'Category'
+    ws1['E2'] = 'Amount'
 
-    i = 4
+    i = 3
     j = 1
 
     for date in trans_data:
@@ -308,18 +329,105 @@ def export(user_id, pswd,path):
            
                     i +=1
                     j +=1
-    
-    
-    chart = DoughnutChart()
 
-    labels = Reference(ws1, min_col= 4, min_row=4, max_row=i-1)
-    data = Reference(ws1, min_col=5, min_row=3, max_row=i-1)
+    # Doughnut Chart --------------------------------------------------
+    chart5 = DoughnutChart()
 
-    chart.add_data(data, titles_from_data=True)
-    chart.set_categories(labels)
-    chart.title = "Expense Distribution"
-    chart.style = 26
-    ws.add_chart(chart, "H1")
+    labels = Reference(ws1, min_col= 4, min_row=3, max_row=i-1)
+    data = Reference(ws1, min_col=5, min_row=2, max_row=i-1)
+
+    chart5.add_data(data, titles_from_data=True)
+    chart5.set_categories(labels)
+    chart5.title = "Expense Distribution"
+    chart5.style = 26
+    ws.add_chart(chart5, "H1")
+
+    
+    # Daily Average Expenses ---------------------------------------------------
+    months = ("Jan","Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep","Oct", "Nov", "Dec")
+
+    ws1['G1'] = 'Months'
+    ws1['H1'] = 'Daily Average'
+    ws1['I1'] = 'Weekly Average'
+    ws1['J1'] = 'Total Expense'
+
+    expense = monthly_expense(user_id)
+    daily_avg = []
+    weekly_avg = []
+
+    for exp in expense:
+        if exp != 0:
+            daily_avg.append(exp/30)
+            weekly_avg.append(exp/7)
+        else:
+            daily_avg.append(0)
+            weekly_avg.append(0) 
+
+    for i in range(1,13):
+        ws1[f'G{i+1}'] = months[i-1]
+        ws1[f'H{i+1}'] = daily_avg[i-1]
+        ws1[f'I{i+1}'] = weekly_avg[i-1]
+        ws1[f'J{i+1}'] = expense[i-1]
+
+
+    # Monthly Balances ----------------------------------------------------------------
+    ws2.merge_cells('A1:B1')
+    cell1 = ws2['A1']
+    cell1.value = f'Balance'
+
+    balance_data = monthly_balance(user_id)
+
+    for i in range(1,13):
+        ws2[f'A{i+1}'] = months[i-1]
+        ws2[f'B{i+1}'] = balance_data[i-1]
+
+
+    # Monthly Balance Bar Chart ----------------------------------------------------
+    chart1 = BarChart()
+
+    labels = Reference(ws2, min_col=1, min_row=2, max_row=13)
+    data = Reference(ws2, min_col=2, min_row=1, max_row=13)
+
+    chart1.add_data(data, titles_from_data=True)
+    chart1.set_categories(labels)
+    chart1.title = "Monthly Balances"
+    chart1.style = 26
+    ws2.add_chart(chart1, "D1")
+
+    # Daily Average Bar Chart -------------------------------------------------------
+    chart2 = BarChart()
+
+    labels = Reference(ws1, min_col=7, min_row=2, max_row=13)
+    data = Reference(ws1, min_col=8, min_row=1, max_row=13)
+    
+    chart2.add_data(data, titles_from_data=True)
+    chart2.set_categories(labels)
+    chart2.title = "Daily Average Expense"
+    chart2.style = 26
+    ch1.add_chart(chart2)
+
+    # Weekly Average Bar Chart ------------------------------------------------------
+    chart3 = BarChart()
+
+    data = Reference(ws1, min_col=9, min_row=1, max_row=13)
+
+    chart3.add_data(data, titles_from_data=True)
+    chart3.set_categories(labels)
+    chart3.title = "Weekly Average Expense"
+    chart3.style = 26
+    ch2.add_chart(chart3)
+
+    # Monthly Expense Chart ----------------------------------------------------------
+    chart4 = BarChart()
+
+    data = Reference(ws1, min_col=10, min_row=1, max_row=13)
+    chart4.add_data(data, titles_from_data=True)
+    chart4.set_categories(labels)
+    chart4.title = "Monthly Expense"
+    chart4.style = 26
+    ch3.add_chart(chart4)
+
+    
 
     filename = 'Transactions.xlsx'
 
@@ -328,7 +436,6 @@ def export(user_id, pswd,path):
     ws.protection.password = f'{pswd}'
 
     # Getting the user's download folder and saving it there
-    downloads = os.path.join(os.path.expanduser('~'), 'Downloads')
     wb.save(f'{path}\{filename}')
           
 
